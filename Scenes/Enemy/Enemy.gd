defe06b7b0735f4e1846b4c21a660d01768fdf89
Tooddles <ejhall2004@gmail.com>
2024-03-12 10:54:25 -0400

    Enemy Movement Done
    Also added a starting position for the player (0,0) because the enemy needed it.

--- Scenes/Enemy/Enemy.gd
+++ Scenes/Enemy/Enemy.gd
@@ -1,19 +1,20 @@
 extends CharacterBody2D
 
 
-const SPEED = 150
+const SPEED = 100
 const homebase_pos = Vector2(0,0)
-const distance_to_see_player = 1200
+const distance_to_see_player = 300
 
 func _ready():
 	position = Vector2(500, 500)
 
 
 func _physics_process(delta):
-	if abs($Player.position.length() - position.length()) > distance_to_see_player:
+	var player_pos = get_parent().get_node("Player").position
+	if abs(player_pos.length() - position.length()) > distance_to_see_player:
 		position = position.move_toward(homebase_pos, delta * SPEED)
 	else:
-		position = position.move_toward($Player.position, delta * SPEED)
+		position = position.move_toward(player_pos, delta * SPEED)
 	
 	
 	
