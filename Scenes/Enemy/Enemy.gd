8dcaff1aa234003bc1f0aa5f582112f15910f884
Tooddles <ejhall2004@gmail.com>
2024-03-12 11:40:17 -0400

    Enemy attack sort-of done
    The enemy will try to attack the player when close, but the attack doesn't actually do anything (since the player doesn't have health yet)
    Fixed an issue with the enemy movement

--- Scenes/Enemy/Enemy.gd
+++ Scenes/Enemy/Enemy.gd
@@ -4,17 +4,30 @@ extends CharacterBody2D
 const SPEED = 100
 const homebase_pos = Vector2(0,0)
 const distance_to_see_player = 300
+const attack_range = 70
+const health = 4 #unused right now
+const damage = 2 #unused right now
 
 func _ready():
 	position = Vector2(500, 500)
+	$AttackTimer.start()
 
 
 func _physics_process(delta):
 	var player_pos = get_parent().get_node("Player").position
-	if abs(player_pos.length() - position.length()) > distance_to_see_player:
+	if (player_pos - position).length() > distance_to_see_player:
 		position = position.move_toward(homebase_pos, delta * SPEED)
 	else:
 		position = position.move_toward(player_pos, delta * SPEED)
+	
+	if ($AttackTimer.time_left == 0):
+		if (player_pos - position).length() <= attack_range:
+			#send signal to player that the enemey is attacking and how much damage it does?
+			print("Enemy is attacking") #temporary
+			$AttackTimer.start()
+			
+			
+		
 	
 	
 	
