992c63a4bedf8d156b6013cfe0fd22ba217d9ec4
Peter Ouchakov <161096242+ouchakovpeter@users.noreply.github.com>
2024-03-14 13:51:53 -0400

    Add Menu
    Basic Menu was added.
    Settings menu is also added.
    Can be expanded on in the future

---
+++ Scenes/Menus/MainMenu/MainMenu.gd
@@ -0,0 +1,10 @@
+extends Control
+
+func _on_play_pressed():#settings button
+	get_tree().change_scene_to_file("res://Prototyping/PrototypeGame.tscn")
+
+func _on_settings_pressed(): #settings button
+	get_tree().change_scene_to_file("res://Scenes/Menus/Settings/Settings.tscn")
+
+func _on_quit_pressed():
+	get_tree().quit()
