da2bdcf88d5ed2e2f4aae95a5287211900214fb3
usomarriba <usomarriba@kx.com>
2024-03-18 19:28:54 -0400

    Add hover tile (todo - only shown in build mode.

--- Scenes/WorldMap/TileMapController.gd
+++ Scenes/WorldMap/TileMapController.gd
@@ -5,8 +5,15 @@ to take effect.
 """
 extends TileMap
 
+@export var hover_tile_source_id: int
+@export var hover_tile_atlas_coords: Vector2
+
 var wall_scene = load("res://Scenes/WorldStructures/CaveWall.tscn")
 var wall_scene_id
+
+# Keep track of the currently highlighted tile. Before updating, stores the previous
+# one so it can be removed without having to scan the whole tile map for it.
+var cur_hover_tile_coords = null  # Starts null since nothing highlighted
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -19,7 +26,12 @@ func _process(delta):
 	pass
 
 func _physics_process(delta):
-	pass
+	var global_mouse_pos = get_global_mouse_position()
+	var tile_coords = local_to_map(global_mouse_pos)
+	var tile_cell = get_cell_source_id(1, tile_coords)
+	
+	update_hover_tile(tile_coords)
+	
 	#var mouse_pos = get_global_mouse_position()
 	#var tile_coords = local_to_map(mouse_pos)
 	#var tile_cell = get_cell_source_id(1, tile_coords)
@@ -28,6 +40,14 @@ func _physics_process(delta):
 	#print(tile_cell)
 	# erase_cell(0, tile_coords)
 	
+func update_hover_tile(new_hover_tile_coords):
+	# Remove old hover tile (if not null, i.e none yet)
+	if cur_hover_tile_coords:
+		# -1 source_id (3rd argument) means to erase the tile at tile_coords on layer 2
+		set_cell(2, cur_hover_tile_coords, -1)  
+	# Set new hover tile
+	set_cell(2, new_hover_tile_coords, hover_tile_source_id, hover_tile_atlas_coords)
+	cur_hover_tile_coords = new_hover_tile_coords  # Update current hover tile coords
 
 func _on_player_build_requested(global_mouse_pos):
 	# Create new building - hardcoded for now to be a wall
