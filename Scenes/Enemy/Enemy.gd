e4b52fd5862f1ea13b900740a5cfa85b443cef42
Tooddles <ejhall2004@gmail.com>
2024-03-24 15:28:43 -0400

    Enemy Attack Improved
    Updated the enemy melee attack so it is similar to the player's.

--- Scenes/Enemy/Enemy.gd
+++ Scenes/Enemy/Enemy.gd
@@ -9,7 +9,7 @@ var moving = true
 
 const homebase_pos = Vector2(0,0)
 const distance_to_see_player = 300
-const attack_range = 75
+const attack_range = 150
 var health = 3
 const damage = 2
 
@@ -23,8 +23,10 @@ func _physics_process(delta):
 	if moving:
 		# change enemy movement accordingly
 		if (player.position - position).length() > distance_to_see_player:
+			look_at(homebase_pos)
 			velocity += ((homebase_pos - position).normalized() * accel * delta)
 		else:
+			look_at(player.position)
 			velocity += ((player.position - position).normalized() * accel * delta)
 		velocity = velocity.limit_length(MAX_SPEED)
 		move_and_slide()
@@ -33,24 +35,22 @@ func _physics_process(delta):
 
 # Method for receiving damage
 func take_damage(amount):
-	if $TakeDamageTimer.time_left == 0:
-		health = health - amount
-		$TakeDamageTimer.start()
-		if (health <= 0):
-			# create mobdrop on enemy death
-			var mobdrop = resource_scene.instantiate()
-			mobdrop.init("mobdrop", "res://Assets/Resources/mobdrop_resource.png")
-			mobdrop.position = position
-			get_parent().add_child(mobdrop)
-			# remove enemy
-			queue_free()
+	health = health - amount
+	if (health <= 0):
+		# create mobdrop on enemy death
+		var mobdrop = resource_scene.instantiate()
+		mobdrop.init("mobdrop", "res://Assets/Resources/mobdrop_resource.png")
+		mobdrop.position = position
+		get_parent().add_child(mobdrop)
+		# remove enemy
+		queue_free()
 
 # handle enemy attack when possible
 func _on_attack_timer_timeout():
 	if (player.position - position).length() <= attack_range:
 		# when hitting player, reset speed (since they need to stop for a successful hit)
 		velocity = Vector2.ZERO
-		player.hit(damage)
+		$EnemyHead.start_attack()
 		# force enemy to stop moving after hit
 		moving = false
 		await get_tree().create_timer(1.0).timeout
@@ -60,5 +60,10 @@ func _on_attack_timer_timeout():
 
 func handle_collision(collision: KinematicCollision2D):
 	var collider = collision.get_collider()
-	if collider.name == "Head":
-		take_damage(collider.get_melee_damage())
+	if collider != null:
+		if collider.name == "Head":
+			if $TakeDamageTimer.time_left == 0:
+				take_damage(collider.get_melee_damage())
+				$TakeDamageTimer.start()
+
+
