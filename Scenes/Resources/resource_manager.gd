c8bffc1482f7a2a01f07de0169b3f7565179dd7b
Psioniczz <teddy.alhouwayek@gmail.com>
2024-03-24 13:07:13 -0400

    Added Resource Manager + Spawners
    -Made a resource manager to keep track of overall values (for HUD purposes + purchases)
    -Made Dirt and Leaves Resource Spawners

---
+++ Scenes/Resources/resource_manager.gd
@@ -0,0 +1,22 @@
+extends Node2D
+
+var dirt_amount = 0
+var stone_amount = 0
+var leaves_amount = 0
+var wood_amount = 0
+
+var mobdrops_amount = 0
+
+func resource_picked_up(type):
+	if type == "dirt":
+		dirt_amount += 1
+	elif type == "stone":
+		stone_amount += 1
+	elif type == "leaves":
+		leaves_amount += 1
+	elif type == "wood":
+		wood_amount += 1
+	elif type == "mobdrops":
+		mobdrops_amount += 1
+	else:
+		print("Unknown resource picked up")
