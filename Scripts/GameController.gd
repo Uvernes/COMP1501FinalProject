3e26afe83dd59a16e55bd4072e8a63251ed469ab
Psioniczz <teddy.alhouwayek@gmail.com>
2024-03-31 23:09:37 -0400

    Revert "Enemy Spawner Created"
    This reverts commit 90030c5b535cfe39374b2911d2faa16a1916dd5c.

--- Scripts/GameController.gd
+++ Scripts/GameController.gd
@@ -3,6 +3,7 @@ Game controller script. Highest level for orchestrating the game logic.
 Recieves signals and delegates tasks accordingly.
 Some tasks include: 
 -Population (handled here)
+-Enemy spawning (handled here for now)
 -Resource management (delegated)
 
 The controller should also manage the calls to updating the UI.
@@ -10,9 +11,22 @@ The controller should also manage the calls to updating the UI.
 
 extends Node2D
 
+@export var enemy_scene: PackedScene
+
+const min_distance_from_player = 200
+const min_player_distance_from_base = 1000
+
+const min_spawn_distance_from_base = 400
+const max_spawn_distance_from_base = 1000
+const min_spawn_distance_from_player = 250 #should be far enough that the player can never see an enemy spawn
+const max_spawn_distance_from_player = 500
 
 const respawn_price = 10
 
+var random = RandomNumberGenerator.new()
+
+var spawn_location_around_base
+var spawn_location_around_player
 var player
 var homebase
 
@@ -25,6 +39,39 @@ func _ready():
 	$HUD.connect("health_button_pressed", handle_upgrade.bind(0))
 	$HUD.connect("stamina_button_pressed", handle_upgrade.bind(1))
 	$HUD.connect("melee_dmg_button_pressed", handle_upgrade.bind(2))
+	$EnemySpawnTimerForBase.start()
+	$EnemySpawnTimerForPlayer.start()
+	
+func _process(_delta):
+	if($EnemySpawnTimerForBase.time_left == 0):
+		var distance = random.randf_range(min_spawn_distance_from_base, max_spawn_distance_from_base)
+		var direction = Vector2(randf_range(-1,1),randf_range(-1,1))
+		var spawn_position = direction * distance
+		
+		spawn_position = $WorldMap.move_spawn_tile_to_cell_at_pos(spawn_position)
+		if(spawn_position - player.position).length() > min_distance_from_player && (spawn_position - homebase.position).length() > min_spawn_distance_from_base:
+			if $WorldMap.can_spawn_mob() == true:
+				var enemy = enemy_scene.instantiate()
+				enemy.position = spawn_position
+				enemy.add_to_group("Enemy")
+				add_child(enemy)
+				$EnemySpawnTimerForBase.start()
+				
+	if player.position.length() > min_player_distance_from_base:
+		if $EnemySpawnTimerForPlayer.time_left == 0:
+			var distance = random.randf_range(min_spawn_distance_from_player, max_spawn_distance_from_player)
+			var direction = Vector2(randf_range(-1,1),randf_range(-1,1))
+			var spawn_position = player.position + (direction * distance)
+			
+			spawn_position = $WorldMap.move_spawn_tile_to_cell_at_pos(spawn_position)
+			if(spawn_position - player.position).length() > min_distance_from_player:
+				if $WorldMap.can_spawn_mob() == true:
+					var enemy = enemy_scene.instantiate()
+					enemy.position = spawn_position
+					enemy.add_to_group("Enemy")
+					enemy.get_node("EnemyHead").add_to_group("EnemyHeads") #used to determine if head is touching homebase
+					add_child(enemy)
+					$EnemySpawnTimerForPlayer.start()
 
 # Handle player build request and delegate accordingly
 func _on_player_build_requested(global_mouse_pos, build_id):
@@ -66,13 +113,13 @@ func handle_player_death():
 	else:
 		#game over
 		print("Player died and could not respawn: Game Over")
-		get_tree().change_scene_to_file("res://Scenes/Menus/MainMenu/MainMenu.tscn")
+		get_tree().quit()
 
 
 func handle_homebase_death():
 	#game over
 	print("Population reached zero: Game Over")
-	get_tree().change_scene_to_file("res://Scenes/Menus/MainMenu/MainMenu.tscn")
+	get_tree().quit()
 
 func handle_upgrade(type):
 	if $ResourceManager.check_upgrade_cost(10) == true:
@@ -83,3 +130,4 @@ func handle_upgrade(type):
 		elif type == 2:
 			player.increase_melee_damage(1)
 		$HUD.update_all_resources($ResourceManager.resources)
+	
