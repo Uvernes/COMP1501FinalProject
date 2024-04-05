6758ebe1b814d36a3bc3885f1c9260fd39b95718
usomarriba <usomarriba@kx.com>
2024-04-04 21:00:36 -0400

    Create nxn game map allowing for transition between rooms and account for edges of the world. (some extra things need to be completed)

--- Scenes/GameMap/GameMap.gd
+++ Scenes/GameMap/GameMap.gd
@@ -16,11 +16,22 @@ extends Node2D
 var Player = load("res://Scenes/Player/Player.gd")
 
 # Add all possible rooms here!
-const room_scene_paths = 	[
-	"res://Scenes/GameMap/Rooms/example_room_1.tscn",
-	"res://Scenes/GameMap/Rooms/example_room_2.tscn",
+const all_room_scene_paths = 	[
+	"res://Scenes/GameMap/Rooms/template_room.tscn",
 	]
+	
+# A 2D array of the rooms making up the game map for the current playthrough.
+# Entries are scene paths
+var game_map: Array
 
+var cur_room_index  # A tuple: (row, col) in map
+var cur_room: TileMap
+var starting_room_index
+
+# Nested dictionary remembering all of the builds currently placed by the player 
+# for each room. Mapping:
+# (Room index) -> Dictionary( (tile position) -> build_index )
+var room_index_to_builds: Dictionary
 #var wall_scene = load("res://Scenes/WorldStructures/CaveWall.tscn")
 #var wall_scene_id
 
@@ -34,6 +45,8 @@ var player_mode
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	# Starting room of the main world is the center row, left most room
+	starting_room_index = [floor((n_rows - 1)/2), 0]
 	init_game_map()
 	
 	return
@@ -74,32 +87,45 @@ func _physics_process(delta):
 		#print(tile_cell)
 		# erase_cell(0, tile_coords)
 
-
-# Init the game map to be num_rows x num_cols in dimension.
-# All assumed to be 60x60 rooms for now
 func init_game_map():
 	for i in n_rows:
+		game_map.append([])
 		for j in n_cols:
 			# Select and instance room to place at rooms[i][j]
 			# For now, starting room is in the top left corner. TODO - have it be in the center   
-			var room_scene_path
-			if i==0 and j==0:
-				room_scene_path = room_scene_paths[0]
-			else:
-				room_scene_path = room_scene_paths.pick_random()
-			var room_scene_instance = load(room_scene_path).instantiate()
-			# Calculate position based on size on row, col position,
-			# and size of tile cells (num cells assumed to be 60 for now
-			# tile_set_size should be the same for all rooms (64x64)
-			var tile_set_size = room_scene_instance.tile_set.tile_size 
-			var room_length_n_tiles = 60  # Again, num cells assumed to be 60 for now
-			room_scene_instance.position = Vector2(
-				room_length_n_tiles * tile_set_size.x * i,
-				room_length_n_tiles * tile_set_size.y * j
-			)
-			print(room_scene_instance.position)
-			add_child(room_scene_instance)
+			game_map[i].append(all_room_scene_paths.pick_random())
 	
+	# First room of non-tutorial map entered from the left
+	init_new_room(starting_room_index, "left") 
+
+
+
+# OLD - completely open world version
+# Init the game map to be num_rows x num_cols in dimension.
+# All assumed to be 60x60 rooms for now
+#func init_game_map():
+	#for i in n_rows:
+		#for j in n_cols:
+			## Select and instance room to place at rooms[i][j]
+			## For now, starting room is in the top left corner. TODO - have it be in the center   
+			#var room_scene_path
+			#if i==0 and j==0:
+				#room_scene_path = room_scene_paths[0]
+			#else:
+				#room_scene_path = room_scene_paths.pick_random()
+			#var room_scene_instance = load(room_scene_path).instantiate()
+			## Calculate position based on size on row, col position,
+			## and size of tile cells (num cells assumed to be 60 for now
+			## tile_set_size should be the same for all rooms (64x64)
+			#var tile_set_size = room_scene_instance.tile_set.tile_size 
+			#var room_length_n_tiles = 60  # Again, num cells assumed to be 60 for now
+			#room_scene_instance.position = Vector2(
+				#room_length_n_tiles * tile_set_size.x * i,
+				#room_length_n_tiles * tile_set_size.y * j
+			#)
+			#print(room_scene_instance.position)
+			#add_child(room_scene_instance)
+	#
 	# For 
 
 
@@ -197,4 +223,83 @@ func _on_player_mode_changed(new_mode):
 		## -1 source_id (3rd argument) means to erase the tile at tile_coords on layer 2
 			#set_cell(2, cur_hover_tile_coords, -1) 
 			#cur_hover_tile_coords = null  # No tile longer hovered
+
+
+# Initialize the room just entered (either at start of the game or when entered from a dif. room).
+# Initialization involves:'
+# -Create room instance
+# -Have exits open only where valid and closed elsewhere
+# -Have player enter room from direction they were travelling
+# -Create any pre-existing builds the player has in the room entered (i.e if revisiting a room)
+func init_new_room(room_index, entrance):
+	# Create instance of room just entered
+	cur_room_index = room_index
+	var cur_room_scene_path = game_map[cur_room_index[0]][cur_room_index[1]]
+	cur_room = load(cur_room_scene_path).instantiate()
+	add_child(cur_room)
+	
+	# By default all exits are closed. Remove the closing for all exits that are valid
+	# (e.g can't enter the right-side exit if in a room on the right-most side of the map)
+	print(cur_room.get_node("ClosedExits").modulate)
+	cur_room.get_node("ClosedExits").modulate.a = 1  # Alpha changed in inspector for dinstinguishing
+	print(cur_room_index)
+	if cur_room_index[0] > 0:
+		cur_room.get_node("ClosedExits/up").queue_free()
+	if cur_room_index[0] < n_rows - 1:
+		cur_room.get_node("ClosedExits/down").queue_free()
+	if cur_room_index[1] > 0:
+		cur_room.get_node("ClosedExits/left").queue_free()
+	if cur_room_index[1] < n_cols - 1:
+		cur_room.get_node("ClosedExits/right").queue_free()
+	# Special case. Exit open if in the starting room of the main map. This is since there are 
+	# tutorial rooms to the left of this room.
+	if cur_room_index == starting_room_index :
+		cur_room.get_node("ClosedExits/left").queue_free()
+
+	# Have player start in room at the specified entrance
+	get_node("Player").global_position = \
+		cur_room.get_node("Entrances").get_node(entrance).global_position
+	cur_room.get_node("Entrances").hide()  # Entrances are just markers for the inspector, can hide
+
+	# TODO - restore any pre-existing builds
+
+# When player exits a room, delete the current room scene and change to the next room.
+func handle_room_exit(direction):
+	# Free the previous room
+	cur_room.queue_free()
+	
+	# One of "left", "right", "up" or "down" based on direction player exited.
+	# e.g if exited to the right then they enter the new room from the left
+	var entrance 
+	
+	# Compute new room index and entrance
+	var new_room_index = cur_room_index
+	if direction == "up":
+		new_room_index[0] -= 1
+		entrance = "down"
+	elif direction == "down":
+		new_room_index[0] += 1
+		entrance = "up"
+	elif direction == "left":
+		new_room_index[1] -= 1
+		entrance = "right"
+	elif direction == "right":
+		new_room_index[1] += 1
+		entrance = "left"
+	
+	# Sanity check error handling (but shouldn't occur)
+	if new_room_index[0] < 0:
+		new_room_index[0] = 0
+		entrance = direction  # Simulates staying in room
+	if new_room_index[0] > n_rows - 1:
+		new_room_index[0] = n_rows - 1
+		entrance = direction  
+	if new_room_index[1] < 0:
+		new_room_index[1] = 0
+		entrance = direction  
+	if new_room_index[1] > n_cols - 1:
+		new_room_index[1] = n_cols - 1
+		entrance = direction  
+	
+	init_new_room(new_room_index, entrance)
 
---
+++ Scenes/GameMap/Rooms/RoomController.gd
@@ -0,0 +1,42 @@
+"""
+RoomController.gd: 
+This is the common script shared between all rooms.
+Handles logic such as detecting when the player exits the room 
+(looking at Area2D) and from which side they exited.
+"""
+extends TileMap
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+# Detect when player exits the room and from which side, and relay this to
+# the game map controller (parent).
+# Possible exit sides: "left", "right", "up", "down"
+func _on_area_2d_body_exited(body):
+	if not body.is_in_group("Player"):
+		return
+	# Determine which side player exited from
+	var direction
+	var room_width = $Area2D/CollisionShape2D.shape.size.x
+	var room_height = $Area2D/CollisionShape2D.shape.size.y
+	if (body.global_position.x <= 
+			$Area2D/CollisionShape2D.global_position.x - room_width/2):
+		direction = "left"
+	elif (body.global_position.x >= 
+			$Area2D/CollisionShape2D.global_position.x + room_width/2):
+		direction = "right"
+	elif (body.global_position.y <= 
+			$Area2D/CollisionShape2D.global_position.y - room_height/2):
+		direction = "up"
+	else:
+		direction = "down"
+
+	get_parent().handle_room_exit(direction)
