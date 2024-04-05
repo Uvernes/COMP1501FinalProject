1a8d6784d5d612f89ac91f747dea191e150ee5d0
usomarriba <usomarriba@kx.com>
2024-04-05 15:21:37 -0400

    Incorporate gameplay scene.

--- Scenes/Menus/MainMenu/MainMenu.gd
+++ Scenes/Menus/MainMenu/MainMenu.gd
@@ -1,7 +1,7 @@
 extends Control
 
 func _on_play_pressed():#settings button
-	get_tree().change_scene_to_file("res://Prototyping/PrototypeGame.tscn")
+	get_tree().change_scene_to_file("res://Prototyping/Gameplay.tscn")
 
 func _on_settings_pressed(): #settings button
 	get_tree().change_scene_to_file("res://Scenes/Menus/Settings/Settings.tscn")
