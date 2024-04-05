4dffe8659378d1285c1c076cf0cd1af4fafe2108
Peter Ouchakov <ouchakovpeter@gmail.com>
2024-04-05 07:58:08 -0400

    Fixed Pause Menu
    Pause Menu Completed And Fixed

--- Scenes/Menus/Pause/PauseMenu.gd
+++ Scenes/Menus/Pause/PauseMenu.gd
@@ -1,9 +1,14 @@
 extends Control
 
+func _ready():
+	resume()
+
 func resume():
+	hide()
 	get_tree().paused = false
 	
 func pause():
+	show()
 	get_tree().paused = true
 
 func _input(event):
@@ -12,11 +17,12 @@ func _input(event):
 	elif event.is_action_pressed("Escape") and get_tree().paused == true:
 		resume()
 
-func _on_resume_pressed():
+func _on_button_pressed():
 	resume()
 
 func _on_quit_pressed():
 	get_tree().quit()
 
 func _on_menu_pressed():
+	resume()
 	get_tree().change_scene_to_file("res://Scenes/Menus/MainMenu/MainMenu.tscn")
