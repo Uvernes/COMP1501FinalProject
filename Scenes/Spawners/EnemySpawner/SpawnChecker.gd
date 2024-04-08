508bb7ab35362553c1a014e1cc2f8c66dfdde76b
Psioniczz <teddy.alhouwayek@gmail.com>
2024-04-07 22:00:06 -0400

    Spawners now functional

--- Scenes/Spawners/EnemySpawner/SpawnChecker.gd
+++ Scenes/Spawners/EnemySpawner/SpawnChecker.gd
@@ -1,12 +1,19 @@
 extends Area2D
 
 
-var usage_allowed: bool = false
 var collisionList = []
 
 func _ready():
 	position = Vector2(-100000, -100000)
 
 func _on_body_entered(body):
-	if usage_allowed:
-		collisionList.add(body)
+	collisionList.append(body)
+
+func _on_area_entered(area):
+	collisionList.append(area)
+
+func _on_body_shape_entered(body_rid, body, body_shape_index, local_shape_index):
+	collisionList.append(body)
+
+func _on_area_shape_entered(area_rid, area, area_shape_index, local_shape_index):
+	collisionList.append(area)
