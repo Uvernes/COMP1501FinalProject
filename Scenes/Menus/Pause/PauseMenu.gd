22e5546f73ee0ec74091057e7ae0cdf03ab9c988
Peter Ouchakov <ouchakovpeter@gmail.com>
2024-04-08 10:20:49 -0400

    Added Pause menu controls
    pause menu controls

--- Scenes/Menus/Pause/PauseMenu.gd
+++ Scenes/Menus/Pause/PauseMenu.gd
@@ -1,6 +1,7 @@
 extends CanvasLayer
 
 func _ready():
+	$Controls2.hide()
 	resume()
 
 func resume():
@@ -17,8 +18,12 @@ func _input(event):
 	elif event.is_action_pressed("Escape") and get_tree().paused == true:
 		resume()
 
-func _on_button_pressed():
+func _on_resume_pressed():
 	resume()
+
+func _on_controls_pressed():
+	$MarginContainer.hide()
+	$Controls2.show()
 
 func _on_quit_pressed():
 	get_tree().quit()
@@ -26,3 +31,7 @@ func _on_quit_pressed():
 func _on_menu_pressed():
 	resume()
 	get_tree().change_scene_to_file("res://Scenes/Menus/MainMenu/MainMenu.tscn")
+
+func _on_back_pressed():
+	$Controls2.hide()
+	$MarginContainer.show()
