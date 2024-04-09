7410ad6ca658d5a7a0b7aedb3fd1159da2899fbf
usomarriba <usomarriba@kx.com>
2024-04-09 11:00:53 -0400

    brighter torches.

--- Scenes/Placeables/UnbreakableTorch.gd
+++ Scenes/Placeables/UnbreakableTorch.gd
@@ -6,6 +6,16 @@ func _ready():
 	_animated_sprite.play()
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+# Activate torch once player within torch emmision range
+func _on_torch_emission_zone_body_entered(body):
+	#print("Body entered.")
+	#print(body.get_groups())
+	#print(body)
+	if body.is_in_group("Player"):
+		$PointLight2D.enabled = true
+
+
+# Deactivate torch once player outside torch emmision range
+func _on_torch_emission_zone_body_exited(body):
+	if body.is_in_group("Player"):
+		$PointLight2D.enabled = false
